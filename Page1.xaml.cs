ca858052d6fa0fd34bdd363870640cb243226249
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-04-22 17:12:55 +0700

    fix sidebar

--- Page1.xaml.cs
+++ Page1.xaml.cs
@@ -24,5 +24,10 @@ namespace MuseumApp
         {
             InitializeComponent();
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
